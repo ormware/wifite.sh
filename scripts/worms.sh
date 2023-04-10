684feccf5a098006eb156f812bbdc268cb20eb4e
ormware <69240937+ormware@users.noreply.github.com>
2023-04-10 09:40:30 +0200

    Update worms.sh

--- scripts/worms.sh
+++ scripts/worms.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #sudo wifite -i wlan1mon -mac -p 10 --skip-crack --no-pmkid --daemon --clients-only
 sudo wifite -i wlan1mon -mac -p 10 --no-pmkid --skip-crack # -inf --clients-only
-lxterminal -e "sh /home/pi/scripts/worms.sh"
-
+tmux split-window -v "sh ~/scripts/worms.sh"
