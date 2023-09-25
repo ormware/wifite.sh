c075a59bda6c530432e77bcef8c8c1d0b01f45b1
ormware <69240937+ormware@users.noreply.github.com>
2023-09-25 18:01:40 +0200

    Update capturePMKID.sh

--- scripts/capturePMKID.sh
+++ scripts/capturePMKID.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #echo "Scanning channel 11"
 #sudo hcxdumptool -o /home/pi/scripts/hcxdumptool/dump.pcapng -i wlan1mon --enable_status 15 -c 11
-sudo hcxdumptool -o /home/pi/scripts/hcxdumptool/capture.pcapng -i wlan1mon --enable_status=15 --filterlist_ap=/home/pi/scripts/filters/pixel.txt --filtermode=2
+sudo hcxdumptool -o ~/scripts/hcxdumptool/capture.pcapng -i wlan1mon --enable_status=15 --filterlist_ap=~/scripts/filters/pixel.txt --filtermode=2
